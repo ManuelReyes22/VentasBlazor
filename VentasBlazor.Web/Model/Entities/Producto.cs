c59d584aeaa0e99fbe4df386ded518c1f7094d6e
ManuelReyes22 <manuel202eyes>
2026-01-24 10:59:16 -0700

    agregamos connectionstring

---
+++ VentasBlazor.Web/Model/Entities/Producto.cs
@@ -0,0 +1,10 @@
+﻿namespace VentasBlazor.Web.Model.Entities
+{
+    public class Producto
+    {
+        public int Id { get; set; }
+        public string Codigo { get; set; }
+        public string Descripcion { get; set; }
+        public decimal ValorUnitario { get; set; }
+    }
+}
